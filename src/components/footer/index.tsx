6c81f3978ef61d82f930d86de4e992ef0793e72a
Tiago Salgado <tiagosalgado2002@hotmail.com>
2022-02-04 18:51:43 -0300

    publicar

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
             <div>
                 <h2>Precisa de ajuda?</h2>
                 <button onClick={() => router.push("https://api.whatsapp.com/send/?phone=5521990045906&text&app_absent=0")}><FaWhatsapp/>FALE COM A GENTE</button>
-                <p>Fale com a gente no whatsapp. Nossa equipe de atendimento está disponível para te atender de segunda a sexta, de 10:00 ás 19:30H</p>
+                <p>Fale com a gente no whatsapp. Nossa equipe de atendimento está disponível para te atender de segunda a sexta, de 10 ás 16H</p>
                 <span>Desenvolvido por <Link href="https://api.whatsapp.com/send/?phone=5521982436773&text&app_absent=0" passHref>Conect-se</Link></span>
             </div>
         </footer>
